46abbbe06de20eace1d4760753e1e08806de6323
Magesh Kuppan <tkmagesh77@gmail.com>
2025-12-17 11:34:51 +0530

    importing and using standard library packages

--- 01-intro/01-hello-world.go
+++ 01-intro/01-hello-world.go
@@ -2,6 +2,7 @@
 package main
 
 /* import dependency packages */
+import "fmt"
 
 /* package scoped variables / constants / type declarations */
 
@@ -9,7 +10,15 @@ package main
 
 /* main function */
 func main() {
-	print("Hello, World!\n")
+	// print("Hello, World!\n")
+	// println("Hello, World!")
+
+	fmt.Print("Hello, World!\n")
+	// fmt.Println("Hello, World!")
+
+	// fmt.Printf
+	// fmt.Printf("Sum of %d and %d is %d\n", 100, 200, 100+200)
+	fmt.Printf("Sum of %0.2f and %0.2f is %0.2f\n", 100.0, 200.0, 100.0+200.0)
 }
 
 /* other functions */
