33c4dc27555a4e301c362d83c862c72c441703bf
Magesh Kuppan <tkmagesh77@gmail.com>
2025-12-17 14:58:41 +0530

    assignment solutions

--- 05-assignments/13-program.go
+++ 05-assignments/13-program.go
@@ -1,10 +1,23 @@
 package main
 
+import "fmt"
+
 /*
 Accept "start" and "end" from the user
 Print all the prime numbers between "start" and "end" (including start and end)
 */
 
 func main() {
-
+	var start, end int
+	fmt.Println("Enter the start and end :")
+	fmt.Scanln(&start, &end)
+LOOP:
+	for no := start; no <= end; no++ {
+		for i := 2; i <= (no / 2); i++ {
+			if no%i == 0 {
+				continue LOOP
+			}
+		}
+		fmt.Println("Prime No :", no)
+	}
 }
