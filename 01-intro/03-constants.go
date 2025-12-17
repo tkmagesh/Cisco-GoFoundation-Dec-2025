1d23f17d8089497a4c99aabaaf8e6a941b15c89e
Magesh Kuppan <tkmagesh77@gmail.com>
2025-12-17 12:11:49 +0530

    constants

---
+++ 01-intro/03-constants.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// const pi float64 = 22.0 / 7
+
+	// type inference
+	const pi = 22.0 / 7
+
+	// Declare and NOT use it - allowed
+	// fmt.Printf("pi = %f\n", pi)
+}
