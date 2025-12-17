1c6b7a00ddcb965a99b4d77001ad6c8b1bf52ece
Magesh Kuppan <tkmagesh77@gmail.com>
2025-12-17 11:46:08 +0530

    variables

---
+++ 01-intro/02-variables.go
@@ -0,0 +1,111 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*
+		var userName string
+		userName = "Magesh"
+	*/
+
+	// Declaration & initialization
+	/*
+		var userName string = "Magesh"
+	*/
+
+	// type inference
+	/*
+		var userName = "Magesh"
+	*/
+
+	// Using := (for declaration, type inference & initialization)
+	userName := "Magesh"
+	fmt.Printf("Hi %s, Have a nice day!\n", userName)
+
+	// multiple variables
+	/*
+		var x int
+		var y int
+		var str string
+		var result int
+		x = 100
+		y = 200
+		str = "Sum of %d and %d is %d\n"
+		result = x + y
+		fmt.Printf(str, x, y, result)
+	*/
+
+	// combine the declaration of variables of the same type together
+	/*
+		var x, y, result int
+		var str string
+		x = 100
+		y = 200
+		str = "Sum of %d and %d is %d\n"
+		result = x + y
+		fmt.Printf(str, x, y, result)
+	*/
+
+	// combine the declaration of variables together irrespective of their type
+	/*
+		var (
+			x, y, result int
+			str          string
+		)
+		x = 100
+		y = 200
+		str = "Sum of %d and %d is %d\n"
+		result = x + y
+		fmt.Printf(str, x, y, result)
+	*/
+
+	// declaration & initialization together
+	/*
+		var x int = 100
+		var y int = 200
+		var str string = "Sum of %d and %d is %d\n"
+		var result int = x + y
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var x, y int = 100, 200
+		var str string = "Sum of %d and %d is %d\n"
+		var result int = x + y
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x, y   int    = 100, 200
+			str    string = "Sum of %d and %d is %d\n"
+			result int    = x + y
+		)
+		fmt.Printf(str, x, y, result)
+	*/
+
+	// type inference
+	/*
+		var (
+			x, y   = 100, 200
+			str    = "Sum of %d and %d is %d\n"
+			result = x + y
+		)
+		fmt.Printf(str, x, y, result)
+	*/
+
+	/*
+		var (
+			x, y, str = 100, 200, "Sum of %d and %d is %d\n"
+			result    = x + y
+		)
+		fmt.Printf(str, x, y, result)
+	*/
+
+	// Using ":="
+
+	x, y, str := 100, 200, "Sum of %d and %d is %d\n"
+	result := x + y
+	fmt.Printf(str, x, y, result)
+}
