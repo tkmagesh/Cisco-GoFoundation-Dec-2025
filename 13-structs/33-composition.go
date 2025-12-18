c3e7294a0d728321c0caa2edde525116a7991734
Magesh Kuppan <tkmagesh77@gmail.com>
2025-12-18 15:40:55 +0530

    struct composition

---
+++ 13-structs/33-composition.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type Product struct {
+	Id   int
+	Name string
+	Cost float64
+}
+
+type PerishableProduct struct {
+	Product
+	Expiry string
+}
+
+func NewPerishableProduct(id int, name string, cost float64, expiry string) *PerishableProduct {
+	return &PerishableProduct{
+		Product: Product{
+			Id:   id,
+			Name: name,
+			Cost: cost,
+		},
+		Expiry: expiry,
+	}
+}
+
+func main() {
+
+	milk := PerishableProduct{
+		Expiry: "2 days",
+		Product: Product{
+			Id:   100,
+			Name: "Milk",
+			Cost: 50,
+		},
+	}
+
+	// milk := NewPerishableProduct(100, "Milk", 50, "2 Days")
+
+	// fmt.Println("Id :", milk.Product.Id)
+	fmt.Println("Id :", milk.Id)
+
+	fmt.Println("Before applying discount :", Format(milk.Product))
+	ApplyDiscount(&(milk.Product), 10)
+	fmt.Println("After applying discount :", Format(milk.Product))
+}
+
+func Format(p Product) string {
+	return fmt.Sprintf("Id : %d, Name : %q, Cost : %0.2f", p.Id, p.Name, p.Cost)
+}
+
+func ApplyDiscount(pPtr *Product, discountPercentage float64) { // no return result
+	pPtr.Cost = pPtr.Cost * ((100 - discountPercentage) / 100)
+}
