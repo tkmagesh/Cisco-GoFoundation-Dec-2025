ed7cd67ed3016ec36b98b1240ec9ffdd84e27399
Magesh Kuppan <tkmagesh77@gmail.com>
2025-12-19 15:00:56 +0530

    assignment solution

--- 17-concurrency/02-waitgroups/05-assignment.go
+++ 17-concurrency/02-waitgroups/05-assignment.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 /*
 Make  the logic of checking each number prime execute concurrently
@@ -10,13 +13,21 @@ func main() {
 	var start, end int
 	fmt.Println("Enter the start and end :")
 	fmt.Scanln(&start, &end)
-LOOP:
+	wg := &sync.WaitGroup{}
 	for no := start; no <= end; no++ {
-		for i := 2; i <= (no / 2); i++ {
-			if no%i == 0 {
-				continue LOOP
-			}
-		}
-		fmt.Println("Prime No :", no)
+		wg.Add(1)
+		go checkPrime(no, wg)
 	}
+	wg.Wait()
+	fmt.Println("Done")
+}
+
+func checkPrime(no int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return
+		}
+	}
+	fmt.Println("Prime No :", no)
 }
