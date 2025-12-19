6ce63d2645eb867ac1584df6c0264a88203810e4
Magesh Kuppan <tkmagesh77@gmail.com>
2025-12-19 10:10:05 +0530

    type assertion - assignment

--- 15-type-assertion/37-type-assertion.go
+++ 15-type-assertion/37-type-assertion.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+
 	// var x interface{}
 	var x any
 
---
+++ 15-type-assertion/38-assignment.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(10))                      // => 10
+	fmt.Println(sum(10, "20"))                // => 30
+	fmt.Println(sum(10, 20, "30", 40, "abc")) // => 100
+	fmt.Println(sum())                        // => 0
+}
+
+func sum( /* ? */ ) int {
+	/* ? */
+}
