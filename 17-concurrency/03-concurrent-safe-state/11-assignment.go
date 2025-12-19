64f1ce38fb9efcc0ae287591662251a71661d9af
Magesh Kuppan <tkmagesh77@gmail.com>
2025-12-19 15:47:58 +0530

    assignment

---
+++ 17-concurrency/03-concurrent-safe-state/11-assignment.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+/*
+Modify as below
+
+1. Do not print the prime number in "checkPrime()"
+2. Collect all the prime numbers in a collection
+3. After all the prime numbers are identified, print them one after another in the main()
+*/
+
+func main() {
+	var start, end int
+	fmt.Println("Enter the start and end :")
+	fmt.Scanln(&start, &end)
+	wg := &sync.WaitGroup{}
+	for no := start; no <= end; no++ {
+		wg.Add(1)
+		go checkPrime(no, wg)
+	}
+	wg.Wait()
+	fmt.Println("Done")
+}
+
+func checkPrime(no int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return
+		}
+	}
+	fmt.Println("Prime No :", no)
+}
