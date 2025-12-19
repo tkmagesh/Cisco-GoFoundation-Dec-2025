f8dc6faa9f15ac0ec698f87228d11b60e162669f
Magesh Kuppan <tkmagesh77@gmail.com>
2025-12-19 10:22:24 +0530

    assignment solution

--- 15-type-assertion/38-assignment.go
+++ 15-type-assertion/38-assignment.go
@@ -1,14 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println(sum(10))                      // => 10
-	fmt.Println(sum(10, "20"))                // => 30
-	fmt.Println(sum(10, 20, "30", 40, "abc")) // => 100
-	fmt.Println(sum())                        // => 0
+	fmt.Println(sum(10))                                    // => 10
+	fmt.Println(sum(10, "20"))                              // => 30
+	fmt.Println(sum(10, 20, "30", 40, "abc"))               // => 100
+	fmt.Println(sum([]int{10, 20}, []any{"30", 40, "abc"})) // => 100
+	fmt.Println(sum())                                      // => 0
 }
 
-func sum( /* ? */ ) int {
-	/* ? */
+func sum(list ...any) int {
+	var result int
+	for _, item := range list {
+		switch v := item.(type) {
+		case int:
+			result += v
+		case string:
+			if no, err := strconv.Atoi(v); err == nil {
+				result += no
+			}
+		}
+	}
+	return result
 }
