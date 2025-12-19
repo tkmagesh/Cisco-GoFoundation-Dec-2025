99a96771caa1a158fd973fcfbf668643234314e5
Magesh Kuppan <tkmagesh77@gmail.com>
2025-12-19 12:03:02 +0530

    custom errors

---
+++ 16-interfaces/42-custom-errors.go
@@ -0,0 +1,37 @@
+/*
+Create a custom error type (replacing ErrDivideByZero) that has the following information
+	multiplier
+	divisor
+	error message as a string
+*/
+
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrDivideByZero error = errors.New("divide by zero error")
+
+func main() {
+	// divisor := 7
+	divisor := 0
+	q, err := divide(100, divisor)
+	if err == nil {
+		fmt.Printf("Q : %d\n", q)
+	} else if err == ErrDivideByZero {
+		fmt.Println("Do not attempt to divide by zero")
+	} else {
+		fmt.Println("unknown error :", err)
+	}
+}
+
+func divide(multiplier, divisor int) (quotient int, err error) {
+	if divisor == 0 {
+		err = ErrDivideByZero
+		return
+	}
+	quotient = multiplier / divisor
+	return
+}
